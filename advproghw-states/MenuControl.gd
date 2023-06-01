c64df7a7facfc0e79371673bb932a62b9142380f
jonathannister <jnister@eastsideprep.org>
2023-06-01 13:10:54 -0700

    Main Menu Finished
    Implemented as main scene, with quit button, and can be interacted with through arrow keys.

--- advproghw-states/MenuControl.gd
+++ advproghw-states/MenuControl.gd
@@ -1,8 +1,15 @@
 extends Control
 
+func _ready(): 
+	$VBoxContainer/startButton.grab_focus()
+
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Level1.tscn")
 
 
 func _on_desc_button_pressed():
 	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
