74839e6e916c172d4a0217e4021119953eece206
jonathannister <jnister@eastsideprep.org>
2023-06-02 01:40:41 -0700

    Finalize Project

--- advproghw-states/enemy.gd
+++
@@ -1,5 +0,0 @@
-class_name Enemy extends CharacterBody2D
-
-func hit():
-	# Called when your enemy is hit, you probably want to implement it
-	pass
--- advproghw-states/nebula.gd
+++
@@ -1,8 +0,0 @@
-extends CharacterBody2D
-
-func _ready():
-	$AnimatedSprite2D.frame = 0 
-	$AnimatedSprite2D.play("Spin")
-
-func _physics_process(delta):
-	pass
