80f0d26b506526afada4da2d35b569c04f8950a7
jonathannister <jnister@eastsideprep.org>
2023-06-01 23:13:11 -0700

    Working Fireballs
    Fireballs spawn randomly and point in the direction they are flying

--- advproghw-states/Level1.gd
+++ advproghw-states/Level1.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
 var skeleton_scene = preload("res://skeleton.tscn")
+var fireball_scene = preload("res://fireball.tscn")
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var timer = 0
+var skeltimer = 0
+var balltimer = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,13 +15,21 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	timer += delta
-	if timer >= 5.0: 
-		timer = 0
+	skeltimer += delta
+	balltimer += delta
+	# Skeletons
+	if skeltimer >= 5.0: 
+		skeltimer = 0
 		var skeleton1 = skeleton_scene.instantiate()
 		var skeleton2 = skeleton_scene.instantiate()
 		skeleton1.position = Vector2(randf_range(0, 100), randf_range(0, 600))
 		skeleton2.position = Vector2(randf_range(1000, 1200), randf_range(0, 600))
 		add_child(skeleton1)
 		add_child(skeleton2)
-
+	if balltimer >= 1.0: 
+		balltimer = 0 
+		var fireball = fireball_scene.instantiate()
+		fireball.position = Vector2(randf_range(0, 1200), 0)
+		add_child(fireball)
+		
+		
